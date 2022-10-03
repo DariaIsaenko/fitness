08852f47c8847314d7731a2d82a8932ecfbe7b24
DariaIsaenko <dariashkvarkova@gmail.com>
2022-10-03 17:42:34 +0300

    Правки после код-ревью

--- source/js/main.js
+++ source/js/main.js
@@ -8,12 +8,13 @@ const videoButton = document.querySelector('.video__button-play');
 const introDescription = document.querySelector('.intro__description');
 const choicePrices = document.querySelectorAll('.choice-subscription__price');
 const choiceButtons = document.querySelectorAll('.choice-month__button');
+const pricesBlocks = document.querySelectorAll('.choice-subscription__prices');
 
 const trainersSliderBlock = document.querySelector('.fitness-trainers__trainers');
 const reviewsSliderBlock = document.querySelector('.reviews__wrapper-reviews');
 
-
 window.addEventListener('DOMContentLoaded', () => {
+  pricesBlocks.forEach((pricesBlock) => pricesBlock.classList.remove('is-nojs'));
   hiddenParagraph();
   initSlider(trainersSliderBlock, treinersSliderSettings);
   initSlider(reviewsSliderBlock, reviewsSliderSettings);
@@ -91,47 +92,3 @@ function activateElement(element) {
 }
 
 choiceButtons.forEach((element) => element.addEventListener('click', () => activateElement(element)));
-
-
-// // ---------------------------------
-
-// window.addEventListener('DOMContentLoaded', () => {
-
-//   // Utils
-//   // ---------------------------------
-
-//   iosVhFix();
-
-//   // Modules
-//   // ---------------------------------
-
-//   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
-//   // в load следует добавить скрипты, не участвующие в работе первого экрана
-//   // window.addEventListener('load', () => {
-//   // });
-// });
-
-// ---------------------------------
-
-// ❗❗❗ обязательно установите плагины eslint, stylelint, editorconfig в редактор кода.
-
-// привязывайте js не на классы, а на дата атрибуты (data-validate)
-
-// вместо модификаторов .block--active используем утилитарные классы
-// .is-active || .is-open || .is-invalid и прочие (обязателен нейминг в два слова)
-// .select.select--opened ❌ ---> [data-select].is-open ✅
-
-// выносим все в дата атрибуты
-// url до иконок пинов карты, настройки автопрокрутки слайдера, url к json и т.д.
-
-// для адаптивного JS используется matchMedia и addListener
-// const breakpoint = window.matchMedia(`(min-width:1024px)`);
-// const breakpointChecker = () => {
-//   if (breakpoint.matches) {
-//   } else {
-//   }
-// };
-// breakpoint.addListener(breakpointChecker);
-// breakpointChecker();
-
-// используйте .closest(el)
